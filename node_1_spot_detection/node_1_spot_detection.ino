c5a05fa7728746035c2660f36d77579ecbcff90a
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2022-10-02 16:38:30 +0100

    Update node_1_spot_detection.ino

--- node_1_spot_detection/node_1_spot_detection.ino
+++ node_1_spot_detection/node_1_spot_detection.ino
@@ -16,8 +16,8 @@ I do not need the Auto485 library for this to work. other RS485 modules availabl
 
 #define CMRI_ADDR 1 //CMRI node address in JMRI
 
-// Define CMRI connection with 24 inputs and 64 outputs no need for outputs in this case
-CMRI cmri(CMRI_ADDR, 24, 64);
+// Define CMRI connection with 16 inputs and 24 outputs no need for outputs in this case
+CMRI cmri(CMRI_ADDR, 16, 24);
 
 void setup() {
   
