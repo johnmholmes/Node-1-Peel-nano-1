68d0fcf0d0f8231154ea67a1c02ce70ab186a543
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2022-10-02 16:37:15 +0100

    Update node_1_spot_detection.ino

--- node_1_spot_detection/node_1_spot_detection.ino
+++ node_1_spot_detection/node_1_spot_detection.ino
@@ -23,15 +23,23 @@ void setup() {
   
   Serial.begin(19200);                     // Make sure this value is set in JMRI for this node
   
-  for (int i=2; i<14; i++)  {
-          pinMode(i, INPUT_PULLUP);        // define sensor shield pins 2 to 13 as inputs
-        } 
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
+    pinMode(6, INPUT_PULLUP);
+    pinMode(7, INPUT_PULLUP);
+    pinMode(8, INPUT_PULLUP);
+    pinMode(9, INPUT_PULLUP);
+    pinMode(10, INPUT_PULLUP);
+    pinMode(11, INPUT_PULLUP);
+    pinMode(12, INPUT_PULLUP);
 
     pinMode(A0, INPUT_PULLUP);
     pinMode(A1, INPUT_PULLUP);
-    pinMode(A2, INPUT_PULLUP);             // not used
-    pinMode(A3, INPUT_PULLUP);             // not used
-    pinMode(A4, INPUT_PULLUP);             // not used
+    pinMode(A2, INPUT_PULLUP);
+    pinMode(A3, INPUT_PULLUP);
+    pinMode(A4, INPUT_PULLUP);
     pinMode(A5, INPUT_PULLUP);             // not used
     pinMode(A6, INPUT_PULLUP);             // not used
     pinMode(A6, INPUT_PULLUP);             // not used
@@ -42,18 +50,21 @@ void loop(){
    cmri.process();
 
    cmri.set_bit(0, !digitalRead(2));   // S1 = address 1001 in JMRI
-   cmri.set_bit(1, !digitalRead(3));   
-   cmri.set_bit(2, !digitalRead(4));   
-   cmri.set_bit(3, !digitalRead(5));   
-   cmri.set_bit(4, !digitalRead(6));   
-   cmri.set_bit(5, !digitalRead(7));   
-   cmri.set_bit(6, !digitalRead(8));   
-   cmri.set_bit(7, !digitalRead(9));  
-   cmri.set_bit(8, !digitalRead(10));  
-   cmri.set_bit(9, !digitalRead(11));   
-   cmri.set_bit(10, !digitalRead(12));
-   cmri.set_bit(11, !digitalRead(13));  
-   cmri.set_bit(12, !digitalRead(A0)); 
-   cmri.set_bit(13, !digitalRead(A1));  // S16 = address 1016 in JMRI
+   cmri.set_bit(1, !digitalRead(3));   // S2 = address 1002 in JMRI   
+   cmri.set_bit(2, !digitalRead(4));   // S3 = address 1003 in JMRI   
+   cmri.set_bit(3, !digitalRead(5));   // S4 = address 1004 in JMRI   
+   cmri.set_bit(4, !digitalRead(6));   // S5 = address 1005 in JMRI   
+   cmri.set_bit(5, !digitalRead(7));   // S6 = address 1006 in JMRI   
+   cmri.set_bit(6, !digitalRead(8));   // S7 = address 1007 in JMRI   
+   cmri.set_bit(7, !digitalRead(9));   // S8 = address 1008 in JMRI  
+   cmri.set_bit(8, !digitalRead(10));  // S9 = address 1009 in JMRI  
+   cmri.set_bit(9, !digitalRead(11));  // S10 = address 1010 in JMRI   
+   cmri.set_bit(10, !digitalRead(12)); // S11 = address 1011 in JMRI
+
+   cmri.set_bit(11, !digitalRead(A0)); // S12 = address 1012 in JMRI  
+   cmri.set_bit(12, !digitalRead(A1)); // S13 = address 1013 in JMRI 
+   cmri.set_bit(13, !digitalRead(A2)); // S14 = address 1014 in JMRI
+   cmri.set_bit(14, !digitalRead(A3)); // S14 = address 1015 in JMRI
+   cmri.set_bit(15, !digitalRead(A4)); // S14 = address 1016 in JMRI
 }
    
